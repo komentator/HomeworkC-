8327ad3eed1945c1c4f8f544ad0e46ed15a2ede8
Артюшенко Александр <zasranechamozur@gmail.com>
2022-10-06 23:06:11 +0300

    all

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿//  Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите три числа ->");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+if(b > max) 
+{
+    max = b;
+}
+if(c > max)
+{
+    max = c;
+}
+Console.WriteLine($"Максимальное число {max}");
---
+++ Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+  Console.WriteLine("Введите число -> ");
+  int num = Convert.ToInt32(Console.ReadLine());
+
+  if(num < 0)
+  {
+    Console.WriteLine("Введите число которое больше нуля ");
+  }
+  int count = 2;
+ while (count <= num)
+ {
+    if (count%2 == 0)
+    {Console.WriteLine(count + "" );}
+ 
+ count++;
+ }
